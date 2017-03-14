1116b86e37afa90e23438789f64301c22578e5d6
elim2513 <8elim@stpatrick.org>
2017-03-14 10:46:18 -0500

    Add files via upload

--- path.js
+++ path.js
@@ -1,20 +1,39 @@
 //Your code below
 function buildPath(p,n){
   let path=[];
-  for(let a=0;){
+  for(){
     let r = Math.random()
     if(r<=p){
-      path[a]= false
+      path[]= false
     }
     else{
-      path[a]= true
+      path[]= true
     }
   }
   return path;
 }
-
-
-
+function stroll(path,index){
+  if(index==path.length-1){
+    return 1;
+  }
+  else if(path[index]==false){
+    return 0;
+  }
+  else{
+    stroll(path,index+1);
+  }
+}
+function rsj(path,index){
+  if(path.length-1==index){
+    return 1;
+  }
+  else if(path[index]==false  ||  index>=path.length){
+    return 0;
+  }
+  else{
+    return rsj(path,index+1)  + rsj(path,index+2)  + rsj(path,index+5);
+  }
+}
 //Test code
 let testpath1 = buildPath(0.10,15);
 let testpath2 = buildPath(0.50,15);
